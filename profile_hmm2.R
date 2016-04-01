581aa5dd7425032bb8a08b0d613c766c02ae28d2
make999 <ikmake9@gmail.com>
2016-04-01 18:13:05 +0800

    add vertices

--- profile_hmm2.R
+++ profile_hmm2.R
@@ -5,81 +5,42 @@ vertices_d = NULL
 comp_emis_prob <- function(vec, letter) {
   
   sum = 0
-  for (v in vec) {
-    if (v == letter) {
-      sum = sum + 1
+  no_let = length(vec)
+  
+  
+    for (v in vec) {
+      if (v == letter) {
+        sum = sum + 1
+      }
     }
-  }
- sum/length(vec)
+  
+  sum/no_let
 }
+
+
 
 emis_prob <-function(vec) {
   c(comp_emis_prob(vec,"A"), comp_emis_prob(vec,"T"), comp_emis_prob(vec,"C"),comp_emis_prob(vec,"G"))
 }
 
-add_vertex <-function(vertices_m, vertices_d, vertices_i, v, num, vec_data) {
+add_vertex <-function(vertices, v, num, vec_data) {
 
-  st = paste(v, toString(num))
+  st = paste(v, toString(num), sep="")
   
-  em_vec1 = emis_prob(vec_data)
-  
-  if(v == "m") {
-    if(!is.element(st, vertices_m)) {
-      vertices_m = c(vertices_m, st, em_vec1)
-    }
-  }else if(v == "d") {
-    if(!is.element(v1, vertices_d)) {
-      vertices_d = c(vertices_d, st, em_vec1)
-    }
-  }else if(v == "i") {
-    if(!is.element(v1, vertices_i)) {
-      vertices_i = c(vertices_i, st, em_vec1)
-    }
+  if(v == "d" && !is.element(st, vertices)) {
+    vertices = c(vertices, st)
+  } else if(v == "i" && !is.element(st, vertices)) {
+    vertices = cbind(vertices,c( st, comp_emis_ins_wrapper(vec_data)))
   }
-  
-  # v1_l = unlist(strsplit(v1,""))[1]
-  # v2_l = unlist(strsplit(v2,""))[1]
-  # 
-  # v1_d = unlist(strsplit(v1,""))[2]
-  # v2_d = unlist(strsplit(v2,""))[2]
-  # 
-  # 
-  # em_vec1 = emis_prob(matrixData[,v1_d])
-  # em_vec2 = emis_prob(matrixData[,v2_d])
-  # 
-  # if(v1_l == "m") {
-  #   if(!is.element(v1, vertices_m)) {
-  #     vertices_m = c(vertices_m, v1, em_vec1)
-  #   }
-  # }else if(v1_l == "d") {
-  #   if(!is.element(v1, vertices_d)) {
-  #     vertices_d = c(vertices_d, v1, em_vec1)
-  #   }
-  # }else if(v1_l == "i") {
-  #   if(!is.element(v1, vertices_i)) {
-  #     vertices_i = c(vertices_i, v1, em_vec1)
-  #   }
-  # }
-  # 
-  # 
-  # if(v2_l == "m") {
-  #   if(!is.element(v2, vertices_m)) {
-  #     vertices_m = c(vertices_m, v2, em_vec2)
-  #   }
-  # }else if(v2_l == "d") {
-  #   if(!is.element(v2, vertices_d)) {
-  #     vertices_d = c(vertices_d, v2, em_vec2)
-  #   }
-  # }else if(v2_l == "i") {
-  #   if(!is.element(v2, vertices_i)) {
-  #     vertices_i = c(vertices_i, v2, em_vec2)
-  #   }
-  # }
-  
+  else if(!is.element(st, vertices)) {
+    em_vec1 = emis_prob(vec_data)
+    vertices = cbind(vertices, c(st, em_vec1))
+  }
+
   vertices
 }
 
-make_edge <- function(vec, st1, n1, st2, n2, tr_prob) {
+make_edge <- function(st1, n1, st2, n2, tr_prob) {
 
   res = c(paste(st1, toString(n1), sep=""), 
             paste(st2, toString(n2), sep=""), tr_prob)
@@ -235,26 +196,27 @@ profile_hmm2 <- function(matrixData, dashes) {
         if(dashes[i] >= 3 && i > 1) {} 
         else if(dashes[i] < 3 && dashes[i] > 0) {
             
-            edges = cbind(edges, make_edge("m", lmn, "i", lmn+1, comp_mch_to_ins(matrixData[,i], insert_mat)))
-            edges = cbind(edges, make_edge("d", lmn, "i", ldn+1, comp_del_to_ins(matrixData[,i], insert_mat)))
-            
-           
-            
+            edges = cbind(edges, make_edge("m", lmn, "i", lmn, comp_mch_to_ins(matrixData[,i], insert_mat)))
+            edges = cbind(edges, make_edge("d", lmn, "i", ldn, comp_del_to_ins(matrixData[,i], insert_mat)))
             lin = i+1
         }
-        else if(dashes[i] == 0 ) {
-            
+        else if(dashes[i] == 0 ) {  
             edges = cbind(edges, make_edge("m", lmn, "i", lmn, comp_mch_to_ins(matrixData[,i], insert_mat)))
             lin = lmn
         }
+        
+        vertices_i = add_vertex(vertices_i, "i", lmn, insert_mat)
     } 
     else if(dashes[i+1] < 3 && dashes[i+1] > 0) {
     
         if(dashes[i] >= 3 && i > 1) { #if previous state is insertion
+
           edges = cbind(edges, make_edge("i", lin, "m", i+1, comp_ins_to_mch(insert_mat, matrixData[,i+1])))
           edges = cbind(edges, make_edge("i", lin, "d", i+1, comp_ins_to_mch(insert_mat, matrixData[,i+1])))
           edges = cbind(edges, make_edge("m", lmn, "m", i+1, comp_mch_ins_mch(matrixData[,lmn],insert_mat, matrixData[,i+1])))
           
+          vertices_m = add_vertex(vertices_m, "m", i+1, matrixData[,i+1])
+          vertices_d = add_vertex(vertices_d, "d", i+1, matrixData[,i+1])
           
           lmn = i+1
           ldn = i+1
@@ -268,16 +230,18 @@ profile_hmm2 <- function(matrixData, dashes) {
             edges = cbind(edges, make_edge("d", ldn,"m", i+1, tr_prob))
             edges = cbind(edges, make_edge("d", ldn,"d", i+1, tr_prob))
             
-            add_vertex(vertices_m, "m", i+1, matrixData[,i+1])
-            add_vertex(vertices_d, "d", i+1)
-            
             lmn = i + 1
             ldn = lmn
         } 
         else if(dashes[i] == 0) {
             edges = cbind(edges, make_edge("m", lmn, "m", i+1, tr_prob))
+            
         }
         edges = cbind(edges, make_edge("m", lmn, "d", i+1, tr_prob))
+        
+        vertices_m = add_vertex(vertices_m, "m", i+1, matrixData[,i+1])
+        vertices_d = add_vertex(vertices_d, "d", i+1, matrixData[,i+1])
+        
         lmn = i+1
         ldn = i+1
     } 
@@ -295,7 +259,36 @@ profile_hmm2 <- function(matrixData, dashes) {
             edges = cbind(edges, make_edge("m", lmn, "m", i+1, tr_prob)) 
             lmn = i+1
         }
+      
+        vertices_m = add_vertex(vertices_m, "m", i+1, matrixData[,i+1])
     } 
   }
-  edges
+  list(edges, vertices_i, vertices_d, vertices_m)
 }
+
+
+comp_emis_ins <- function(vec, letter) {
+  
+  sum = 0
+  no_let = length(vec)
+  
+  for (i in 1:length(vec[,1])) {
+      for (j in 1:length(vec[1,])) {
+        if(vec[i,j] == letter) {
+          sum = sum + 1
+          no_let = no_let + 1
+        } else if(vec[i,j] != "-") {
+          no_let = no_let + 1
+        }
+      }
+    }
+  sum/no_let
+}
+
+comp_emis_ins_wrapper <- function(vec) {
+  
+  c(comp_emis_ins(vec,"A"), comp_emis_ins(vec,"T"), comp_emis_ins(vec,"C"),comp_emis_ins(vec,"G"))
+  
+}
+
+
